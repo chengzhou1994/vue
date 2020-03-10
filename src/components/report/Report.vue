45f54143f2d41e5d3e37138a3cde09525f1a0f40
成舟 <ichengzhou@icloud.com>
2020-03-11 00:17:51 +0800

    完成数据报表

---
+++ src/components/report/Report.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片试图区域 -->
+    <el-card>
+      <!-- 第二步为ECharts准备一个具备大小（宽高）的Dom -->
+      <div id="main" style="width: 750px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+// 第一步导入
+import echarts from "echarts";
+import _ from "lodash";
+
+export default {
+  data() {
+    return {
+      // 需要合并的数据
+      options: {
+        title: {
+          text: "用户来源"
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "#E9EEF3"
+            }
+          }
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: "value"
+          }
+        ]
+      }
+    };
+  },
+  created() {},
+  methods: {},
+  // 此时页面上的元素已经渲染完毕
+  async mounted() {
+    // 第三步 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById("main"));
+    // 获取折线图的数据
+    const { data: res } = await this.$http.get("reports/type/1");
+    if (res.meta.status !== 200) {
+      return this.$message.error("获取折线图数据失败！");
+    }
+
+    // 第四步 服务器返回的数据与需要合并的选项进行合并
+    const result = _.merge(res.data, this.options);
+    // 第五步 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result);
+  }
+};
+</script>
+
+<style>
+</style>
